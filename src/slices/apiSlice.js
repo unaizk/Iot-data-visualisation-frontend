2707670a6dbdc35ecd831b84dc02836a731e7b3c
root <root@Unaislaptop>
2024-03-24 18:18:53 +0530

    Changed the cors

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({baseUrl: 'https://iot-data-backend.unaizk.com', credentials: 'include'})
+const baseQuery = fetchBaseQuery({baseUrl: 'http://65.2.26.112:5000', credentials: 'include'})
 
 export const apiSlice = createApi({
     baseQuery,
