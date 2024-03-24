c7fcf6fbf1e7ef2e54849906d2b85d4d1f97b7cb
root <root@Unaislaptop>
2024-03-24 20:18:11 +0530

    Changed the cors

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port : 3000,
     proxy : {
       '/api' : {
-        target : 'https://iot-data-backendd.unaizk.com',
+        target : 'https://iot-data-backend.unaizk.com',
         changeOrigin : true
       }
     }
