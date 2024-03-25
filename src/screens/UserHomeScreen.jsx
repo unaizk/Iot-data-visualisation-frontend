b728b574c895aa6cccfa7f0a0b0bde4b3aa7db59
root <root@Unaislaptop>
2024-03-25 19:10:46 +0530

    showing data through analyzing the user role

--- src/screens/UserHomeScreen.jsx
+++ src/screens/UserHomeScreen.jsx
@@ -16,13 +16,13 @@ const UserHomeScreen = () => {
     getIotDataFunction();
   }, []);
 
-  console.log(iotData);
+  console.log(iotData,'llllllllllllllll');
   return (
     <div className="chart flex flex-col items-center mt-8 mb-8">
       <h1 className="text-center mb-5 font-black text-2xl">IOT DATA VISUALISATION</h1>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        
-        <div className="col-4 mt-5 ">
+        {iotData.map((data) => (
+          <div className="col-4 mt-5 ">
           <h1 className="text-center">TWO DIGIT NUMBER</h1>
           {isLoading && <Loader />}
           <Chart
@@ -43,90 +43,8 @@ const UserHomeScreen = () => {
             width="500"
           />
         </div>
-        <div className="col-4 mt-5">
-          <h1 className="text-center">THREE DIGIT NUMBER</h1>
-          {isLoading && <Loader />}
-          <Chart
-            options={{
-              colors: ["#000000", "#808080"],
-              chart: { id: "basic-bar" },
-              xaxis: {
-                categories: iotData?.threeDigitNumber?.map((data) => data.time),
-              },
-            }}
-            series={[
-              {
-                name: "series-1",
-                data: iotData?.threeDigitNumber?.map((data) => data.data),
-              },
-            ]}
-            type="bar"
-            width="500"
-          />
-        </div>
-        <div className="col-4 mt-5">
-          <h1 className="text-center">FOUR DIGIT NUMBER</h1>
-          {isLoading && <Loader />}
-          <Chart
-            options={{
-              colors: ["#000000", "#808080"],
-              chart: { id: "basic-bar" },
-              xaxis: {
-                categories: iotData?.fourDigitNumber?.map((data) => data.time),
-              },
-            }}
-            series={[
-              {
-                name: "series-1",
-                data: iotData?.fourDigitNumber?.map((data) => data.data),
-              },
-            ]}
-            type="bar"
-            width="500"
-          />
-        </div>
-        <div className="col-4 mt-5">
-          <h1 className="text-center">TWO DIGIT LETTER</h1>
-          {isLoading && <Loader />}
-          <Chart
-            options={{
-              colors: ["#000000", "#808080"],
-              chart: { id: "basic-bar" },
-              xaxis: {
-                categories: iotData?.twoDigitLetter?.map((data) => data.data),
-              },
-            }}
-            series={[
-              {
-                name: "series-1",
-                data: iotData?.twoDigitLetter?.map((data) => data.time),
-              },
-            ]}
-            type="bar"
-            width="500"
-          />
-        </div>
-        <div className="col-4 mt-5">
-          <h1 className="text-center">FOUR DIGIT LETTER</h1>
-          {isLoading && <Loader />}
-          <Chart
-            options={{
-              colors: ["#000000", "#808080"],
-              chart: { id: "basic-bar" },
-              xaxis: {
-                categories: iotData?.fourDigitLetter?.map((data) => data.data),
-              },
-            }}
-            series={[
-              {
-                name: "series-1",
-                data: iotData?.fourDigitLetter?.map((data) => data.time),
-              },
-            ]}
-            type="bar"
-            width="500"
-          />
-        </div>
+        ))}
+        
       </div>
     </div>
   );
