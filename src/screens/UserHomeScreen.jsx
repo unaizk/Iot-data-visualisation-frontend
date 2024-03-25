ad00fef8798ed8a2bf1b98d74e9234d5bed309c9
root <root@Unaislaptop>
2024-03-25 19:25:43 +0530

    make changes

--- src/screens/UserHomeScreen.jsx
+++ src/screens/UserHomeScreen.jsx
@@ -2,49 +2,47 @@ import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { useIotDataMutation } from "../slices/userApiSlice";
 import Loader from "../components/Loader";
+
 const UserHomeScreen = () => {
   const [getIotData, { isLoading }] = useIotDataMutation();
-
-  const [iotData, setIotData] = useState({});
+  const [iotData, setIotData] = useState([]);
 
   useEffect(() => {
     const getIotDataFunction = async () => {
       const res = await getIotData().unwrap();
-      console.log(res,'fffff');
+      console.log(res, 'fffff');
       setIotData(res);
     };
     getIotDataFunction();
   }, []);
 
-  
   return (
     <div className="chart flex flex-col items-center mt-8 mb-8">
       <h1 className="text-center mb-5 font-black text-2xl">IOT DATA VISUALISATION</h1>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        
-          <div className="col-4 mt-5 ">
-          <h1 className="text-center">TWO DIGIT NUMBER</h1>
-          {isLoading && <Loader />}
-          <Chart
-            options={{
-              colors: ["#000000", "#808080"],
-              chart: { id: "basic-bar" },
-              xaxis: {
-                categories: [],
-              },
-            }}
-            series={[
-              {
-                name: "series-1",
-                data: [],
-              },
-            ]}
-            type="bar"
-            width="500"
-          />
-        </div>
-        
-        
+        {iotData?.map((categoryData, index) => (
+          <div key={index} className="col-4 mt-5">
+            <h1 className="text-center">{categoryData[0]?.name}</h1>
+            {isLoading && <Loader />}
+            <Chart
+              options={{
+                colors: ["#000000", "#808080"],
+                chart: { id: `basic-bar-${index}` },
+                xaxis: {
+                  categories: categoryData?.map(item => item.data), // Assuming 'data' is the property containing the x-axis values
+                },
+              }}
+              series={[
+                {
+                  name: "series-1",
+                  data: categoryData?.map(item => item.time), // Assuming 'data' is the property containing the y-axis values
+                },
+              ]}
+              type="bar"
+              width="500"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
